bfd5c5e0442ccb3b69ea6083c8b453501d0ef92f
suna-ji <jisuna314@gmail.com>
2023-08-08 23:14:50 +0900

    Add entity test code

--- src/main/java/com/example/datajpa/entity/Member.java
+++ src/main/java/com/example/datajpa/entity/Member.java
@@ -19,12 +19,20 @@ public class Member {
     private int age;
 
     // 외래키가 있는 Member가 연관관계의 주인
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "team_id")
     private Team team;
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
     }
 
     // 연관관계 메서드
--- src/main/java/com/example/datajpa/entity/Team.java
+++ src/main/java/com/example/datajpa/entity/Team.java
@@ -1,22 +1,20 @@
 package com.example.datajpa.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.data.annotation.Id;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Getter @Setter
+@Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(of = {"id", "name"})
 public class Team {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     @Column(name = "team_id")
     private Long id;
     private String name;
